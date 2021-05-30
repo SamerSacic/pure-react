01c149d02f61664fc48ed2c3336df69524bf2b92
SamerSacic <samer.sacic@gmail.com>
2021-05-31 00:27:57 +0200

    Creating React components and how to preform JSX syntax inside them

--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -1,17 +1,115 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function HelloWorld() {
+  return (
+    <div>
+      <Hello /> <World />!
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Hello() {
+  return <span>Hello</span>;
+}
+
+function World() {
+  return <span>World</span>;
+}
+
+// How React render JSX syntax
+function HelloWithCreateElement() {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("div", {}, "Child1"),
+    React.createElement(
+      "div",
+      {},
+      "Child2",
+      React.createElement("div", {}, "Child2_child")
+    )
+  );
+}
+
+// How React render components with props
+function SongName(props) {
+  // return <span className="song-name">{props.song.name}</span>
+  // compiled TO:
+  return React.createElement(
+    "span",
+    { className: "song-name" },
+    props.song.name
+  );
+}
+
+// How to avoid that components must return enclosing tag
+// To avoid that you use React.Fragment
+// Or with JSX <> </> empty JSX tags
+function NameCells() {
+  // Each component must return elements in a single enclosing tag
+  // User React.Fragment to avoid that
+  //   return (
+  //     <React.Fragment>
+  //       <td>Some Column 1 </td>
+  //       <td>Some Column 2</td>
+  //     </React.Fragment>
+  //   );
+
+  // React.Fragment in JSX syntax looks like this:
+  return (
+    <>
+      <td>Some Column 1</td>
+      <td>Some Column 2</td>
+    </>
+  );
+}
+
+// How to insert pure JS wth JSX components
+// Since this will be compiled to JS, the JS inside the braces must return the expression
+// An expression produce a value
+// These are expressions:
+// 1 + 2
+// buttonLabel
+// aFunctionCall()
+// aFunctionName
+
+// Statement do not produce the value and cannot be used in JSX
+// Here an example of statements:
+// const a = 5
+// if(true) { 17; }
+// while(1 < 7) { i++ }
+function SubmitButton() {
+  const buttonLabel = "Submit";
+  return <button>{buttonLabel}</button>;
+}
+
+// How to use the conditional logic if we cannot use if in JSX?
+// There a couple of ways:
+function ValidateIndicator() {
+  const isValid = true;
+
+  // Ternary operator
+  // return <span>{isValid ? "valid" : "not-valid"}</span>;
+
+  // Boolean operators
+  return (
+    <span>
+      {isValid && "valid"}
+      {!isValid && "not-valid"}
+    </span>
+  );
+}
+
+// JSX require to close every tag
+// DO THIS:
+// return <br />;
+// return <input type="text" ... />
+// return <li>text</li>
+
+// DO NOT DO THIS:
+// return <br>;
+// return <input type="text" ...>
+// return <li>text
+
+ReactDOM.render(<HelloWorld />, document.querySelector("#root"));
